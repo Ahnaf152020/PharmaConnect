99bdcf26698709b6b1f362e0c8f748fa0ae87edc
kalvin-chakma <kalvinchakma7@gmail.com>
2023-12-14 19:48:26 +0600

    Add StartPage and LoginPage

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:pharma_connect/pages/startPage.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StartPage(),
+    ));
---
+++ lib/pages/LoginPage.dart
@@ -0,0 +1,341 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import "package:iconsax/iconsax.dart";
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  int activeIndex = 0;
+
+  @override
+  void initState() {
+    Timer.periodic(const Duration(seconds: 2), (timer) {
+      setState(() {
+        activeIndex++;
+
+        if (activeIndex == 5) activeIndex = 0;
+      });
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  height: 300,
+                  child: Stack(children: [
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: AnimatedOpacity(
+                        opacity: activeIndex == 0 ? 1 : 0,
+                        duration: Duration(
+                          seconds: 2,
+                        ),
+                        curve: Curves.linear,
+                        child: Image.asset(
+                          "assets/images/image1.png",
+                          height: 400,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: AnimatedOpacity(
+                        opacity: activeIndex == 1 ? 1 : 0,
+                        duration: Duration(seconds: 2),
+                        curve: Curves.linear,
+                        child: Image.asset(
+                          "assets/images/image2.png",
+                          height: 400,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: AnimatedOpacity(
+                        opacity: activeIndex == 2 ? 1 : 0,
+                        duration: Duration(seconds: 2),
+                        curve: Curves.linear,
+                        child: Image.asset(
+                          "assets/images/image3.png",
+                          height: 400,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: AnimatedOpacity(
+                        opacity: activeIndex == 3 ? 1 : 0,
+                        duration: Duration(seconds: 2),
+                        curve: Curves.linear,
+                        child: Image.asset(
+                          "assets/images/image4.png",
+                          height: 400,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: AnimatedOpacity(
+                        opacity: activeIndex == 4 ? 1 : 0,
+                        duration: Duration(seconds: 2),
+                        curve: Curves.linear,
+                        child: Image.asset(
+                          "assets/images/image5.png",
+                          height: 400,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ]),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(0.0),
+                    labelText: 'Email',
+                    hintText: 'Username or e-mail',
+                    labelStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14.0,
+                    ),
+                    prefixIcon: Icon(
+                      Iconsax.user,
+                      color: Colors.black,
+                      size: 18,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.grey.shade200, width: 3),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    floatingLabelStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18.0,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 1.5),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  cursorColor: Colors.black,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(0.0),
+                    labelText: 'Password',
+                    hintText: 'Password',
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14.0,
+                    ),
+                    labelStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    prefixIcon: Icon(
+                      Iconsax.key,
+                      color: Colors.blue,
+                      size: 18,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.grey.shade200, width: 3),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    floatingLabelStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18.0,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 1.5),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Forgot Password?',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                MaterialButton(
+                  onPressed: () {},
+                  height: 15,
+                  color: Colors.lightBlueAccent,
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 1,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Text(
+                          "Or continur with",
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 1,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        width: 30,
+                        height: 30,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(color: Colors.white),
+                            image: const DecorationImage(
+                              image: AssetImage("assets/images/google.png"),
+                              fit: BoxFit.cover,
+                            ),
+                            color: Colors.grey[200]),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        width: 30,
+                        height: 30,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: Colors.white,
+                          ),
+                          image: DecorationImage(
+                            image: AssetImage("assets/images/fb.png"),
+                            fit: BoxFit.cover,
+                          ),
+                          color: Colors.grey[200],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an account?',
+                      style: TextStyle(
+                          color: Colors.grey.shade600,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Register',
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
