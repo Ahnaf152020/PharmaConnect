e02e1606fc186e9b0a1d743bb0c36263071d3d45
Turjo24 <turjodutta023@gmail.com>
2023-12-25 20:13:32 +0600

    initial commit

---
+++ lib/firebase.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+//import 'package:fluttertoast/fluttertoast.dart';
+class FirebaseAuthService{
+  final FirebaseAuth _auth =FirebaseAuth.instance;
+
+
+  Future<User?> signUpWithEmailAndPassword(String username,String email,String password) async{
+
+    try{
+      UserCredential credential=await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      return credential.user;
+    }  on FirebaseAuthException catch(e){
+
+        if(e.code == 'email-already-in-use'){
+          print('The e-mail address is already in use');
+
+        }else{
+          print('An error occurred :${e.code}');
+        }
+      }
+
+
+    return null;
+  }
+
+  Future<User?> signInWithEmailAndPassword(String email,String password) async{
+
+    try{
+      UserCredential credential=await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return credential.user;
+    } on FirebaseAuthException catch(e){
+
+      if(e.code == 'user-not-found' || e.code =='wrong password'){
+        print('Invalid e-mail or password');
+      }else{
+        print('An error occured: ${e.code}');
+      }
+
+    }
+
+    return null;
+  }
+   void forgetPassword(String email) {
+     _auth.sendPasswordResetEmail(email: email).then((value) {
+       var Get;
+       Get.back();
+       print('Email Sent, We have sent password reset email');
+     }).catchError((e) {
+       print("Error in sending password reset email is $e");
+     });
+   }
+}
+
---
+++ lib/register.dart
@@ -0,0 +1,184 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'firebase.dart';
+
+class Myregister extends StatefulWidget {
+  const Myregister({Key? key}) : super(key: key);
+
+  @override
+  State<Myregister> createState() => _MyregisterState();
+}
+
+class _MyregisterState extends State<Myregister> {
+
+  final FirebaseAuthService _auth = FirebaseAuthService();
+
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+
+  @override
+  void dispose(){
+    _usernameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/register.png'), fit: BoxFit.cover)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(children: [
+          Container(
+            padding: EdgeInsets.only(left: 35.0, top: 30.0),
+            child: Text(
+              'CREATE A ACCOUNT',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.2,
+                    left: 30,
+                    right: 35),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _usernameController,
+                      decoration: InputDecoration(
+                          hintText: 'Full Name',
+                          filled: true,
+                          fillColor: Colors.grey,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                          hintText: 'E-mail',
+                          filled: true,
+                          fillColor: Colors.grey,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+
+                controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: 'Password',
+                          filled: true,
+                          fillColor: Colors.grey,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Age',
+                          filled: true,
+                          fillColor: Colors.grey,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      decoration: InputDecoration(
+                          hintText: 'City',
+                          filled: true,
+                          fillColor: Colors.grey,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Phone Number',
+                          filled: true,
+                          fillColor: Colors.grey,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                            radius: 30,
+                            backgroundColor: Colors.cyanAccent,
+                            child: IconButton(
+                              onPressed: () {
+                                Navigator.pushNamed(context, 'LoginPage');
+                              },
+                              icon: Icon(Icons.arrow_back),
+                            )),
+
+           GestureDetector(
+             onTap: _signup,
+
+               child: Container(
+                   decoration: BoxDecoration(
+                     color: Colors.black,
+                     borderRadius: BorderRadius.circular(30.0),
+                   ),padding: const EdgeInsets.all(25),
+                 child:Text('Sign up',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,),)
+                     ),
+             ),
+
+
+                      ],
+                    ),
+                  ],
+                )),
+          )
+        ]),
+      ),
+    );
+  }
+
+  void _signup() async{
+
+
+    String username =_usernameController.text;
+    String email =_emailController.text;
+    String password =_passwordController.text;
+
+    User? user= await _auth.signUpWithEmailAndPassword(username,email,password);
+
+    if(user!=null){
+      print("succesfully done signup");
+    Navigator.pushNamed(context, 'widget');
+
+    }else{
+      print("error occured finish");
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Colors.lightBlueAccent,
+            content: Text(
+              'error occured',
+              style: TextStyle(color: Colors.white),
+            ),
+          );
+        },
+      );
+
+    }
+
+  }
+}
+
