0da3c102acb7190244eb79d8f4641c0e10a9b66b
Sharadindu Dutta <turjodutta023@gmail.com>
2024-01-07 21:55:18 +0600

    fullchatpageworkme

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pharmaconnectbyturjo/chatsearchpage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -49,13 +50,26 @@ class _HomePageState extends State<HomePage> {
                   SizedBox(
                     width: 20,
                   ),
-                  Container(
-                    child: Image.asset(
-                      "assets/icon/chat.png",
-                      height: 30,
-                      fit: BoxFit.cover,
+
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => chatpage()),
+                      );
+                    },
+                    child: Container(
+                      child: Image.asset(
+                        "assets/icon/chat.png",
+                        height: 30,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
+                  )
+
+
+
 
                 ]),
               ),
